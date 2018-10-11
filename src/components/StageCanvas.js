8a01de601aa74816d71369044fb2750d09143495
tomforge <fooguowei3@gmail.com>
2018-10-11 22:08:59 +0800

    Render stage rect and grid

--- src/components/StageCanvas.js
+++ src/components/StageCanvas.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import {Circle, Layer, Stage} from "react-konva";
+import {calculateStageDimensions, generateGrid} from "./stageUtils";
 
 class StageCanvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      dimensions: {
+        width: 9.6,
+        height: 5.18,
+        unit: 'm'
+      },
       dancers: [
         {
           name: "Bob",
@@ -30,12 +36,32 @@ class StageCanvas extends Component {
     }
   }
 
-
   render() {
+    // Canvas dimensions
+    let {width: canvasWidth, height: canvasHeight} = this.props;
+    let stageRect = calculateStageDimensions(canvasWidth, canvasHeight,
+      this.state.dimensions.width, this.state.dimensions.height, 0.05);
+
+    console.log("Stage: ", canvasWidth, canvasHeight);
+
+    let gridLayer = (
+      <Layer>
+        {
+          generateGrid(canvasWidth, canvasHeight, 0.05 * canvasWidth)
+            .map((points, key) => <Line key={key} points={points} stroke={"#514a9d"} opacity={0.5}/>)
+        }
+      </Layer>
+    );
+    let stageLayer = (
+      <Layer>
+        <Rect width={stageRect.width} height={stageRect.height} x={stageRect.tl.x} y={stageRect.tl.y}
+              stroke={"#514a9d"} strokeWidth={3}/>
+      </Layer>
+    );
     return (
-      <Stage preventDefault={true} width={this.props.width} height={this.props.height}
-             scaleX={this.state.scale}
-             scaleY={this.state.scale}>
+      <Stage preventDefault={true} width={canvasWidth} height={canvasHeight}>
+        {gridLayer}
+        {stageLayer}
         <Layer>
           {this.state.dancers.map((dancer, key) => (
             <Circle
