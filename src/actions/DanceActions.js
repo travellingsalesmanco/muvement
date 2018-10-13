48b67ec471e581cce8f143c99b7f170f9727b2dd
tomforge <fooguowei3@gmail.com>
2018-10-13 20:58:11 +0800

    add action creators for validating user-input actions

---
+++ src/actions/DanceActions.js
@@ -0,0 +1,35 @@
+import {ADD_DANCER, EDIT_STAGE_DIMENSIONS} from "../constants/actionTypes";
+
+function isDuplicateDancer(danceId, name, state) {
+  return state.dances[danceId].dancers.includes(name);
+}
+
+export function addDancer(danceId, name) {
+  return (dispatch, getState) => {
+    if (!isDuplicateDancer(danceId, name, getState())) {
+      dispatch({
+        type: ADD_DANCER,
+        danceId: danceId,
+        payload: name
+      })
+    } else {
+      console.log("[ERROR] Duplicate dancer name")
+    }
+  }
+}
+
+export function editStageDimensions(danceId, toEdit) {
+  return dispatch => {
+    if (toEdit.width && toEdit.width <= 0
+      || toEdit.height && toEdit.height <= 0
+      || toEdit.gridSize && toEdit.gridSize < 0) {
+      console.log("[ERROR] Dimensions must be greater than 0")
+    } else {
+      dispatch({
+        type: EDIT_STAGE_DIMENSIONS,
+        danceId: danceId,
+        payload: toEdit
+      })
+    }
+  }
+}
