8ab5a2db5d364499b8d8c208ac7c75ea7f885b21
John Phua <phuajohn0@gmail.com>
2018-10-15 21:17:27 +0800

    Add default frame for addFrame action

--- src/reducers/frames.js
+++ src/reducers/frames.js
@@ -72,9 +72,9 @@ export default (state = [defaultFrame], action) => {
   switch (action.type) {
 
     case ADD_FRAME: {
-      const {index, payload: frame} = action;
+      const {payload: addAt} = action;
       let frames = state.slice();
-      frames.splice(index, 0, frame);
+      frames.splice(addAt, 0, defaultFrame);
       return frames;
     }
     case REMOVE_FRAME: {
