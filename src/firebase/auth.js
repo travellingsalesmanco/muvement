5ba470615773f39b94ef8c7fd10763ff1cbdca82
John Phua <phuajohn0@gmail.com>
2018-10-20 00:05:38 +0800

    Add firebase infrastructure with env var config

---
+++ src/firebase/auth.js
@@ -0,0 +1,38 @@
+import { auth, facebookProvider, googleProvider } from './firebase';
+
+// TODO: handle multiple credentials
+
+export const doSignOut = () => {
+  return auth.signOut();
+};
+
+// Email and Password Methods
+export const doCreateUserWithEmailAndPassword = (email, password) => {
+  return auth.createUserWithEmailAndPassword(email, password);
+};
+
+export const doSignInWithEmailAndPassword = (email, password) => {
+  return auth.signInWithEmailAndPassword(email, password);
+};
+
+export const doPasswordReset = (email) => {
+  return auth.sendPasswordResetEmail(email);
+};
+
+export const doPasswordUpdate = (password) => {
+  return auth.currentUser.updatePassword(password);
+};
+
+
+// Google Methods
+
+
+export const googleSignIn = () => {
+  return auth.signInWithPopup(googleProvider);
+};
+
+// Facebook Methods
+
+export const facebookSignIn = () => {
+  return auth.signInWithPopup(facebookProvider);
+};
