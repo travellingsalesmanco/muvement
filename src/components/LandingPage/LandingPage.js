3342f25472e0f3a74d26d6a4ee389cb911e9475e
Wyin Kok <wyin.kwy@gmail.com>
2018-10-21 23:55:06 +0800

    Edited the landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import { Button, Col, Layout, Row } from 'antd';
 import React from 'react';
 import ReactGA from 'react-ga';
-import GridImage from "../../img/grid-image.svg";
+import GridImage from "../../img/grid-image-portrait.svg";
 import Logo from "../../img/m-logo.svg";
 import Name from "../../img/MuvementLogo.png";
 import './LandingPage.css';
@@ -25,19 +25,14 @@ class LandingPage extends React.Component {
               IN</Button>
           </div>
         </ Header>
+
         <Layout className="landingpage-contents">
           <Content style={{ display: "flex", flexDirection: "column" }}>
-            <Row>
-              <Col className="muvement-name-img" span={12}>
-                <img src={Name} width="250" height="180" alt="muvement name" />
-                <p className="muvement-desc">Create and visualise your formation <br /> without pen and paper</p>
-                <Button className="landing-signup-button" onClick={() => this.props.history.push(`/signup`)}>LETS GET
-                  CREATIVE</Button>
-              </Col>
-              <Col span={12}>
-                <img className="grid-img" src={GridImage} alt="grid image" />
-              </Col>
-            </Row>
+            <img className="muvement-name-img" src={Name} width="250" height="180" alt="muvement name" />
+            <p className="muvement-desc">Create and visualise your formation <br /> without pen and paper</p>
+            <Button className="landing-signup-button" onClick={() => this.props.history.push(`/signup`)}>LETS GET
+              CREATIVE</Button>
+            <img className="grid-img" src={GridImage} alt="grid image" />
           </Content>
         </Layout>
       </Layout>
