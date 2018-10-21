24b310a0810e02b6aa5a031eb52ab4c3930bef77
John Phua <phuajohn0@gmail.com>
2018-10-22 02:04:28 +0800

    Use middleware for firestore writes

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import localForage from 'localforage';
 import {composeWithDevTools} from 'redux-devtools-extension'
+import { firestoreWriter } from "../middleware/firestoreWrite";
 
 localForage.config({
   name: "Muvement",
@@ -18,7 +19,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export default () => {
   const store = createStore(
     persistedReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunk, firestoreWriter))
   );
   const persistor = persistStore(store);
   return { store, persistor }
