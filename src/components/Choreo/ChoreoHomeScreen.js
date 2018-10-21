bd93d5b3851fd39020cc4aefce9b21e782d9f05a
Yu Jia Tay <yujiatay@gmail.com>
2018-10-21 18:10:42 +0800

    Reformat choreo home screen

--- src/components/Choreo/ChoreoHomeScreen.js
+++ src/components/Choreo/ChoreoHomeScreen.js
@@ -1,11 +1,11 @@
+import {Button, Layout, Menu} from 'antd';
 import React from 'react';
-import './ChoreoHomeScreen.css';
-import {Button, Layout, Menu, Row, Col, DatePicker} from 'antd';
-import connect from "react-redux/es/connect/connect";
-import {BrowserRouter as Route} from "react-router-dom";
-import FrameScreen from "../Formation/FrameScreen";
-import ChoreoPicture from "./ChoreoPicture";
+import {connect} from "react-redux";
+import {getDance} from '../../selectors/dance';
 import StageDimForm from "../Formation/StageDimForm";
+import withAuthorization from "../withAuthorization";
+import './ChoreoHomeScreen.css';
+import ChoreoPicture from "./ChoreoPicture";
 import FormationPreviewCards from "./FormationPreviewCards";
 import EditIcon from "../../icons/EditIcon";
 import withAuthorization from "../withAuthorization";
@@ -21,83 +21,50 @@ class ChoreoHomeScreen extends React.Component {
     isButtonActive: 1
   };
 
-  disabledStartDate = (startValue) => {
-    const endValue = this.state.endValue;
-    if (!startValue || !endValue) {
-      return false;
-    }
-    return startValue.valueOf() > endValue.valueOf();
-  }
-
-  disabledEndDate = (endValue) => {
-    const startValue = this.state.startValue;
-    if (!endValue || !startValue) {
-      return false;
-    }
-    return endValue.valueOf() <= startValue.valueOf();
-  }
-
   onChange = (field, value) => {
     this.setState({
       [field]: value,
     });
-  }
-
-  onStartChange = (value) => {
-    this.onChange('startValue', value);
-  }
-
-  onEndChange = (value) => {
-    this.onChange('endValue', value);
-  }
-
-  handleStartOpenChange = (open) => {
-    if (!open) {
-      this.setState({endOpen: true});
-    }
-  }
-
-  handleEndOpenChange = (open) => {
-    this.setState({endOpen: open});
-  }
+  };
 
   buttonClick = (number) => {
     this.setState({isButtonActive: number});
-  }
+  };
 
   render() {
     if (this.props.error) {
       return <div>Invalid dance</div>
     }
     const {Header, Content, Sider} = Layout;
-    const {startValue, endValue, endOpen} = this.state;
+    const navbar = (
+      <Header>
+        <div className="nav-bar">
+          <div>
+            <Button className="backbutton" style={{fontSize: '25px'}} icon="left"
+                    onClick={() => this.props.history.push('/')}/>
+            <span className="backbutton-desc">Dashboard</span>
+          </div>
+          <div className="title">
+            <h3 style={{color: '#fff'}}>{this.props.name}</h3>
+          </div>
+          <div className="right-container">
+            <Menu mode="horizontal" theme="dark">
+              <Menu.Item key="1">
+                <Button icon="edit" ghost/>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Button icon="setting" ghost/>
+              </Menu.Item>
+            </Menu>
+          </div>
+        </div>
+      </Header>
+    );
     return (
       <Layout className="choreo-homescreen-body">
-        <Header>
-          <div className="nav-bar">
-            <div>
-              <Button className="backbutton" style={{fontSize: '25px'}} icon="left"
-                      onClick={() => this.props.history.push('/')}/>
-              <span className="backbutton-desc">Dashboard</span>
-            </div>
-            <div className="title">
-              <h3 style={{color: '#fff'}}>{this.props.name}</h3>
-            </div>
-            <div className="right-container">
-              <Menu mode="horizontal" theme="dark">
-                  <Menu.Item key="1">
-                     <Button icon="edit" ghost/>
-                  </Menu.Item>
-                <Menu.Item key="2">
-                  <Button icon="setting" ghost/>
-                </Menu.Item>
-              </Menu>
-            </div>
-          </div>
-        </Header>
+        {navbar}
         <Layout className="choreo-homescreen-contents">
           <Content style={{display: "flex", flexDirection: "column"}}>
-            {/*<FormationPreviewCards data={["1. Introduction (30 seconds)", "2. Straight Line"]} />*/}
             <FormationPreviewCards frames={this.props.frames} match={this.props.match} danceId={this.props.danceId}/>
           </Content>
           <Sider
@@ -105,17 +72,20 @@ class ChoreoHomeScreen extends React.Component {
             <div className="choreo-homescreen-tabs">
               <button
                 className={this.state.isButtonActive === 1 ? 'choreo-homescreen-activebutton' : 'choreo-homescreen-inactivebutton'}
-                onClick={() => this.buttonClick(1)}>ABOUT</button>
-              <button  className = {this.state.isButtonActive === 2 ? 'choreo-homescreen-activebutton' : 'choreo-homescreen-inactivebutton'}
-                      onClick={() => this.buttonClick(2)}>PERFORMERS</button>
+                onClick={() => this.buttonClick(1)}>ABOUT
+              </button>
+              <button
+                className={this.state.isButtonActive === 2 ? 'choreo-homescreen-activebutton' : 'choreo-homescreen-inactivebutton'}
+                onClick={() => this.buttonClick(2)}>PERFORMERS
+              </button>
             </div>
             {
-                this.state.isButtonActive === 1
+              this.state.isButtonActive === 1
                 ? <div>
-                    <ChoreoPicture />
-                    <h2 className="stagedim-title">STAGE DIMENSION</h2>
-                    <StageDimForm danceId={this.props.danceId} />
-                  </div>
+                  <ChoreoPicture/>
+                  <h2 className="stagedim-title">STAGE DIMENSION</h2>
+                  <StageDimForm danceId={this.props.danceId}/>
+                </div>
                 : <div className="edit-performers">
                   <PerformerList danceId={this.props.danceId}/>
                 </div>
