fbbeb95bf0bc2b6566cd4cf102635bff47a05f23
tomforge <fooguowei3@gmail.com>
2018-10-21 12:12:14 +0800

    update dance(s) reducers for the new structure

--- src/reducers/dances.js
+++ src/reducers/dances.js
@@ -1,21 +1,27 @@
-import {combineReducers} from 'redux'
-import dancers from './dancers'
-import frames from './frames'
-import {ADD_DANCE, EDIT_STAGE_DIMENSIONS, REMOVE_DANCE, RENAME_DANCE} from '../constants/actionTypes';
-import {defaultStageDim, dummyDance} from '../constants/defaults';
+import { combineReducers } from 'redux';
+import { ADD_DANCE, EDIT_STAGE_DIMENSIONS, PUBLISH_DANCE, REMOVE_DANCE, RENAME_DANCE, UNPUBLISH_DANCE } from '../constants/actionTypes';
+import { defaultStageDim } from '../constants/defaults';
+import { dummyDances } from '../constants/dummyData';
+import dancers from './dancers';
+import frames from './frames';
 
-const danceNameReducer = (state = "", action) => {
+const danceNameReducer = (state = "", action) => action.type === RENAME_DANCE ? action.payload : state;
+
+const dancePublishedReducer = (state = false, action) => {
   switch (action.type) {
-    case RENAME_DANCE:
-      return action.payload;
+    case PUBLISH_DANCE:
+      return true;
+    case UNPUBLISH_DANCE:
+      return false;
     default:
-      return state
+      return state;
   }
-};
+}
+
 const stageDimReducer = (state = defaultStageDim, action) => {
   switch (action.type) {
     case EDIT_STAGE_DIMENSIONS: {
-      const {payload: editedDimensions} = action;
+      const { payload: editedDimensions } = action;
       return {
         ...state,
         ...editedDimensions
@@ -28,6 +34,7 @@ const stageDimReducer = (state = defaultStageDim, action) => {
 
 const danceReducer = combineReducers({
   name: danceNameReducer,
+  published: dancePublishedReducer,
   stageDim: stageDimReducer,
   dancers,
   frames
@@ -35,25 +42,40 @@ const danceReducer = combineReducers({
 
 // Dances
 // TODO: Remove dummy dance when deploying
-export default (state = [dummyDance], action) => {
+export default (state = dummyDances, action) => {
   switch (action.type) {
     case ADD_DANCE: {
-      const {payload: danceToAdd} = action;
-      let newDances = state.slice();
-      newDances.push(danceToAdd);
-      return newDances;
+      const { danceId, dance } = action.payload;
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          danceId: dance
+        },
+        myDances: [...state.myDances, danceId]
+      }
     }
     case REMOVE_DANCE: {
-      const {payload: indexToRemove} = action;
-      let dances = state.slice();
-      dances.splice(indexToRemove, 1);
-      return dances;
+      const { payload: idToRemove } = action;
+      const { [idToRemove]: _, ...prunedByIds } = state.byId;
+      return {
+        ...state,
+        byId: prunedByIds,
+        myDances: state.myDances.filter(danceId => danceId !== idToRemove)
+      }
     }
-    default:
+    default: {
       if (action.danceId !== null && action.danceId !== undefined) {
-        const {danceId, ...prunedAction} = action;
-        return state.map((dance, idx) => idx === danceId ? danceReducer(dance, prunedAction) : dance);
+        const { danceId, ...prunedAction } = action;
+        return {
+          ...state,
+          byId: {
+            ...state.byId,
+            danceId: danceReducer(state.byId[danceId], prunedAction)
+          }
+        }
       }
       return state;
+    }
   }
 }
