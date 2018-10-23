a5d85b31ad0e59b9e943bc6e5ae252b8e2051ffa
Wyin Kok <wyin.kwy@gmail.com>
2018-10-23 16:19:26 +0800

    Add Performer page for mobile portrait

--- src/components/Home/AddPerformerScreen.js
+++ src/components/Home/AddPerformerScreen.js
@@ -6,6 +6,7 @@ import { addChoreo } from "../../actions/choreoActions";
 import { connect } from 'react-redux';
 import { firestore } from "../../firebase";
 import { defaultStageDim } from "../../constants/defaults";
+import { MinTablet, MobileLandscape, MobilePortrait } from "../ResponsiveUtils/BreakPoint";
 
 class AddPerformerScreen extends React.Component {
   state = {
@@ -49,15 +50,16 @@ class AddPerformerScreen extends React.Component {
     const { Header, Content } = Layout;
     return (
       <Fragment>
+      <MobilePortrait>
         <Header>
           <div className="nav-bar">
-            <div className="back-button">
+            <div className="mp-back-button">
               <Button style={{ fontSize: '25px' }} icon="left" onClick={() => this.props.history.goBack()} />
             </div>
-            <div className="title">
+            <div className="mp-addperf-title">
               <h3 style={{ color: '#fff' }}>ADD PERFORMERS</h3>
             </div>
-            <div className="right-container">
+            <div className="mp-right-container">
               <Menu mode="horizontal" theme="dark">
                 <Menu.Item key="1">
                   <Button icon="setting" ghost />
@@ -67,13 +69,13 @@ class AddPerformerScreen extends React.Component {
           </div>
         </Header>
         <Content>
-          <div className="add-performer-container">
+          <div className="mp-add-performer-container">
             {
               this.state.names.map((performer, key) => (
-                <div key={key} className="performer-field">
+                <div key={key} className="mp-performer-field">
                   <Icon type="user" style={{ color: '#fff', fontSize: '20px' }} />
                   <Input
-                    className="input"
+                    className="mp-input"
                     placeholder="Input performer name"
                     value={this.handleGetName(key)}
                     onChange={(e) => this.handleEditName(e, key)}
@@ -82,12 +84,97 @@ class AddPerformerScreen extends React.Component {
                 </div>
               ))
             }
-            <Button type={"default"} icon="user-add" ghost block className="add-performer-button"
+            <Button type={"default"} icon="user-add" ghost block className="mp-add-performer-button"
                     onClick={this.handleAdd}>Add Performer</Button>
-            <Button type={"default"} block className="next-button"
+            <Button type={"default"} block className="mp-next-button"
                     onClick={this.handleNext}>NEXT</Button>
           </div>
         </Content>
+        </MobilePortrait>
+
+        <MobileLandscape>
+          <Header>
+            <div className="nav-bar">
+              <div className="back-button">
+                <Button style={{ fontSize: '25px' }} icon="left" onClick={() => this.props.history.goBack()} />
+              </div>
+              <div className="ml-addperf-title">
+                <h3 style={{ color: '#fff' }}>ADD PERFORMERS</h3>
+              </div>
+              <div className="right-container">
+                <Menu mode="horizontal" theme="dark">
+                  <Menu.Item key="1">
+                    <Button icon="setting" ghost />
+                  </Menu.Item>
+                </Menu>
+              </div>
+            </div>
+          </Header>
+          <Content>
+            <div className="mp-add-performer-container">
+              {
+                this.state.names.map((performer, key) => (
+                  <div key={key} className="mp-performer-field">
+                    <Icon type="user" style={{ color: '#fff', fontSize: '20px' }} />
+                    <Input
+                      className="mp-input"
+                      placeholder="Input performer name"
+                      value={this.handleGetName(key)}
+                      onChange={(e) => this.handleEditName(e, key)}
+                      onPressEnter={this.handleEditNameConfirm}
+                    />
+                  </div>
+                ))
+              }
+              <Button type={"default"} icon="user-add" ghost block className="mp-add-performer-button"
+                      onClick={this.handleAdd}>Add Performer</Button>
+              <Button type={"default"} block className="mp-next-button"
+                      onClick={this.handleNext}>NEXT</Button>
+            </div>
+          </Content>
+          </MobileLandscape>
+
+          <MinTablet>
+            <Header>
+              <div className="nav-bar">
+                <div className="back-button">
+                  <Button style={{ fontSize: '25px' }} icon="left" onClick={() => this.props.history.goBack()} />
+                </div>
+                <div className="title">
+                  <h3 style={{ color: '#fff' }}>ADD PERFORMERS</h3>
+                </div>
+                <div className="right-container">
+                  <Menu mode="horizontal" theme="dark">
+                    <Menu.Item key="1">
+                      <Button icon="setting" ghost />
+                    </Menu.Item>
+                  </Menu>
+                </div>
+              </div>
+            </Header>
+            <Content>
+              <div className="add-performer-container">
+                {
+                  this.state.names.map((performer, key) => (
+                    <div key={key} className="performer-field">
+                      <Icon type="user" style={{ color: '#fff', fontSize: '20px' }} />
+                      <Input
+                        className="input"
+                        placeholder="Input performer name"
+                        value={this.handleGetName(key)}
+                        onChange={(e) => this.handleEditName(e, key)}
+                        onPressEnter={this.handleEditNameConfirm}
+                      />
+                    </div>
+                  ))
+                }
+                <Button type={"default"} icon="user-add" ghost block className="add-performer-button"
+                        onClick={this.handleAdd}>Add Performer</Button>
+                <Button type={"default"} block className="next-button"
+                        onClick={this.handleNext}>NEXT</Button>
+              </div>
+            </Content>
+            </MinTablet>
       </Fragment>
     );
   }
