781a675813851602d9c56faf348bc3638257514d
John Phua <phuajohn0@gmail.com>
2018-10-24 13:45:54 +0800

    Add imageUrl to choreo

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,7 +1,7 @@
 export const defaultTransition = {
   duration: 0,
   midPoint: 0
-}
+};
 export const defaultFormation = {
   name: "",
   numSeconds: 30,
@@ -13,6 +13,9 @@ export const defaultStageDim = {
   height: 5.18,
   gridSize: 0.5,
   units: 'm'
+};
+export const genDummyImage = (name) => {
+  return "https://dummyimage.com/300x300/000000/fff.jpg&text=" + name.charAt(0);
 };
 export const defaultUI = {
   isPlaying: false,
