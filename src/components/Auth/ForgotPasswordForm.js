84110e0eb61c482cc3061cb5e2e45bdf0ce7abba
John Phua <phuajohn0@gmail.com>
2018-11-01 02:24:16 +0800

    Implement password resets

--- src/components/Auth/ForgotPasswordForm.js
+++ src/components/Auth/ForgotPasswordForm.js
@@ -1,38 +1,65 @@
 import { Form, Input, Icon, Button } from 'antd';
 import React from 'react';
 import './Auth.css';
+import { auth } from "../../firebase";
 
 const FormItem = Form.Item;
 
 class ForgotPasswordForm extends React.Component {
+  state = {
+    loading: false,
+    disabled: false,
+    sent: false,
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
+    this.setState({
+      loading: true,
+      disabled: true,
+    }, () => this.props.form.validateFields((err, values) => {
+      if (err) {
+        this.setState({
+          loading: false,
+          disabled: false,
+        })
+      } else {
         console.log('Received values of form: ', values);
+        auth.doPasswordReset(values.email)
+        this.setState({
+          loading: false,
+          disabled: false,
+          sent: true
+        })
       }
-    });
+    }));
   };
 
   render() {
     const { getFieldDecorator } = this.props.form;
+    const { disabled, loading, sent } = this.state;
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormItem>
-          {getFieldDecorator('email', {
-            rules: [{ required: true, message: 'Please input your email!' }],
-          })(
-            <Input className="auth-input" prefix={<Icon type="mail" />} placeholder="Email" />
-          )}
-        </FormItem>
-        <FormItem>
-          <Button type="primary" htmlType="submit" className="auth-signup-button">
-            SEND EMAIL
-          </Button>
-        </FormItem>
-      </Form>
-    );
+      sent
+        ? <span className={"auth-text"}>You should receive an email if you are registered.</span>
+        : <Form onSubmit={this.handleSubmit}>
+          <FormItem>
+            {getFieldDecorator('email', {
+              rules: [{ required: true, type: 'email', message: 'Please input your email!' }],
+            })(
+              <Input className="auth-input" prefix={<Icon type="mail" />} placeholder="Email" />
+            )}
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit" className="auth-signup-button"
+                    loading={loading}
+                    disabled={disabled}
+            >
+              SEND EMAIL
+            </Button>
+          </FormItem>
+        </Form>
+    )
+      ;
   }
 }
 
--- src/components/Auth/ResetPassword.js
+++ src/components/Auth/ResetPassword.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import './Auth.css';
-import {Button, Layout} from 'antd';
-import connect from "react-redux/es/connect/connect";
-import {BrowserRouter as Route} from "react-router-dom";
+import { Button, Layout } from 'antd';
+import { auth } from '../../firebase';
+import withAuthorization from "../withAuthorization";
 
 class ResetPassword extends React.Component {
+  state = {
+    loading: false,
+    disabled: false,
+    sent: false
+  };
 
+  handleSubmit = () => {
+    this.setState({
+        loading: true,
+        disabled: true,
+      }, () => auth.doPasswordResetInAccount().then(() => this.setState({
+        loading: false,
+        disabled: false,
+        sent: true
+      }))
+    );
+  };
+
+  // TODO: Fix styling issues
   render() {
-       const {Header} = Layout;
+    const { Header } = Layout;
+    const { loading, disabled, sent } = this.state;
     return (
-    <Layout>
+      <Layout>
         <Header className="auth-header">
-            <div>
-              <Button className="auth-backbutton" style={{fontSize: '25px'}} icon="left"
-                      onClick={() => this.props.history.goBack()}/>
-              <span className="auth-backbutton-desc">Setting</span>
-            </div>
+          <div>
+            <Button className="auth-backbutton" style={{ fontSize: '25px' }} icon="left"
+                    onClick={() => this.props.history.goBack()} />
+            <span className="auth-backbutton-desc">Setting</span>
+          </div>
         </Header>
-         <div className="auth-background">
-            <h1 className="auth-forgetpw-title">RESET PASSWORD</h1>
-            <div>
-                <Button className="auth-signup-button" type="primary" htmlType="submit">SAVE</Button>
-            </div>
-         </div>
-     </Layout>
+        <div className="auth-background">
+          <h1 className="auth-forgetpw-title">RESET PASSWORD</h1>
+          <div>
+            {
+              sent
+                ? <span className={"auth-text"}>An email has been sent to you!</span>
+                : <Button className="auth-signup-button" type="primary" htmlType="submit"
+                          onClick={() => this.handleSubmit()}
+                          loading={loading}
+                          disabled={disabled}
+                >Send me an email</Button>
+            }
+          </div>
+        </div>
+      </Layout>
 
 
     );
   }
 }
 
-export default ResetPassword;
+// Auth exists
+const authCondition = (authUser) => !!authUser;
+const failRoute = '/';
+
+export default withAuthorization(authCondition, failRoute)(ResetPassword);
