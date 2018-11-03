4f3b51d293d8710c6514306885d32c4b64a97da2
John Phua <phuajohn0@gmail.com>
2018-11-03 18:22:59 +0800

    Add music function to storage

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -1,6 +1,7 @@
 import { storageRef } from "./firebase";
 
 const choreoImagesRef = storageRef.child('choreoImages');
+const choreoMusicRef = storageRef.child('choreoMusic');
 
 // Returns url for reference if successful
 export const addChoreoImage = (file, choreoId) => {
@@ -14,3 +15,19 @@ export const removeChoreoImage = (choreoId) => {
   let imageRef = choreoImagesRef.child(choreoId + ".jpg");
   return imageRef.delete();
 };
+
+
+// TODO: Check if media encoding is needed
+// Returns url for reference if successful
+export const addChoreoMusic = (file, choreoId) => {
+  let musicRef = choreoMusicRef.child(choreoId);
+  return musicRef.put(file).then((snapshot) => {
+    return snapshot.ref.getDownloadURL();
+  })
+};
+
+export const removeChoreoMusic = (choreoId) => {
+  let musicRef = choreoMusicRef.child(choreoId);
+  return musicRef.delete();
+};
+
