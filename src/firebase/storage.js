c63f17ee568165cadbbb032e987e83c2ed8584f7
John Phua <phuajohn0@gmail.com>
2018-11-03 18:22:50 +0800

    Change dance to choreo

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -3,14 +3,14 @@ import { storageRef } from "./firebase";
 const choreoImagesRef = storageRef.child('choreoImages');
 
 // Returns url for reference if successful
-export const addChoreoImage = (file, danceId) => {
-  let imageRef = choreoImagesRef.child(danceId + ".jpg");
+export const addChoreoImage = (file, choreoId) => {
+  let imageRef = choreoImagesRef.child(choreoId + ".jpg");
   return imageRef.put(file).then((snapshot) => {
     return snapshot.ref.getDownloadURL();
   })
 };
 
-export const removeChoreoImage = (danceId) => {
-  let imageRef = choreoImagesRef.child(danceId + ".jpg");
+export const removeChoreoImage = (choreoId) => {
+  let imageRef = choreoImagesRef.child(choreoId + ".jpg");
   return imageRef.delete();
 };
