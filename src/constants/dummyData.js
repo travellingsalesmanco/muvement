6db771d30f23121df60315fb94c29da65adc65df
John Phua <phuajohn0@gmail.com>
2018-11-04 19:53:15 +0800

    Update demo dummy data

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -1,33 +1,95 @@
 export const demoChoreo = {
-  createdAt: { seconds: 1540148067, nanoseconds: 77000000 },
-  creator: { id: "LrG4PV9PpwgieRuMa5YsSQ2iSUo1", name: "John Phua" },
-  dancers: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
+  createdAt: { seconds: 1541329788, nanoseconds: 157000000 },
+  creator: { id: "ircJhRoOiHPN8NhAeEaqQOZ5ndn1", name: "John Phua" },
+  updatedAt: { seconds: 1541330798, nanoseconds: 342000000 },
   imageUrl: "https://dummyimage.com/300x300/000000/fff.jpg&text=L",
-  formations: [{
-    dancers: [
-      { name: "f", position: [0.5016886187098953, 0.49240121580547114] },
-      { name: "e", position: [0.6148260722728809, 0.3302938196555218] },
-      { name: "d", position: [0.726274907125971, 0.16312056737588654] },
-      { name: "k", position: [0.2737250928740291, 0.1656534954407295] },
-      { name: "g", position: [0.38855116514690985, 0.3302938196555218] },
-      { name: "c", position: [0.7195204322863897, 0.41134751773049644] },
-      { name: "b", position: [0.726274907125971, 0.6519756838905775] },
-      { name: "j", position: [0.27203647416413373, 0.41894630192502536] },
-      { name: "i", position: [0.27541371158392436, 0.6519756838905775] },
-      { name: "a", position: [0.7296521445457615, 0.8343465045592704] },
-      { name: "h", position: [0.2771023302938197, 0.8343465045592704] }
-    ],
-    name: "Landing Demo Formation",
-    duration: 5000,
-    transitionBefore: {
-      duration: 0,
-      midPoint: 0
-    }
-  }],
+  musicUrl: null,
   name: "Landing Demo",
-  published: false,
-  stageDim: { gridSize: 0.5, height: 6, units: "m", width: 9 },
-  updatedAt: { seconds: 1540148283, nanoseconds: 441000000 }
+  published: true,
+  stageDim: { gridSize: 0.5, height: 5.18, units: "m", width: 9.6 },
+  dancers: ["Performer 1", "Performer 2", "Performer 3", "Performer 4", "Performer 5",
+    "Performer 6", "Performer 7", "Performer 8", "Performer 9", "Performer 10", "Performer 11"],
+  formations: [{
+    dancers: [{
+      name: "Performer 10",
+      position: [0.7055555555555556, 0.5873015873015873]
+    }, { name: "Performer 9", position: [0.709837962962963, 0.40476190476190477] }, {
+      name: "Performer 7",
+      position: [0.6088966049382717, 0.3025168025168025]
+    }, { name: "Performer 6", position: [0.4982716049382716, 0.4044044044044044] }, {
+      name: "Performer 4",
+      position: [0.2848302469135803, 0.2030745030745031]
+    }, { name: "Performer 3", position: [0.2933950617283951, 0.38488488488488487] }, {
+      name: "Performer 2",
+      position: [0.28911265432098765, 0.5903045903045903]
+    }, { name: "Performer 11", position: [0.706983024691358, 0.783068783068783] }, {
+      name: "Performer 1",
+      position: [0.2907561728395061, 0.7906334906334906]
+    }, { name: "Performer 5", position: [0.39831018518518524, 0.30740740740740735] }, {
+      name: "Performer 8",
+      position: [0.711604938271605, 0.21465751465751462]
+    }], duration: 2250, name: "", transitionBefore: { duration: 2000, midPoint: 0 }
+  }, {
+    dancers: [{
+      name: "Performer 6",
+      position: [0.4982716049382716, 0.4044044044044044]
+    }, { name: "Performer 7", position: [0.5519165479582148, 0.3993168993168993] }, {
+      name: "Performer 8",
+      position: [0.6129028173472618, 0.4933244599911266]
+    }, { name: "Performer 9", position: [0.6687363485280153, 0.40220403553736883] }, {
+      name: "Performer 11",
+      position: [0.7525417458056347, 0.48574581907915243]
+    }, { name: "Performer 10", position: [0.7186862931307378, 0.4036278036278037] }, {
+      name: "Performer 5",
+      position: [0.4457935660018994, 0.40714084047417376]
+    }, { name: "Performer 4", position: [0.38929368471035153, 0.49640812974146303] }, {
+      name: "Performer 3",
+      position: [0.3359148464704021, 0.40684970684970684]
+    }, { name: "Performer 2", position: [0.27618451250395687, 0.40843774177107506] }, {
+      name: "Performer 1",
+      position: [0.23549817980373527, 0.49474199474199493]
+    }], duration: 2000, name: "", transitionBefore: { duration: 2000, midPoint: 0 }
+  }, {
+    dancers: [{
+      name: "Performer 6",
+      position: [0.4982716049382716, 0.4044044044044044]
+    }, { name: "Performer 7", position: [0.5519165479582148, 0.3993168993168993] }, {
+      name: "Performer 8",
+      position: [0.6129028173472618, 0.4933244599911266]
+    }, { name: "Performer 5", position: [0.4457935660018994, 0.40714084047417376] }, {
+      name: "Performer 4",
+      position: [0.3863307217473886, 0.4854257187590521]
+    }, { name: "Performer 3", position: [0.35072966128521693, 0.5935506935506935] }, {
+      name: "Performer 2",
+      position: [0.29099932731877165, 0.6720156053489386]
+    }, { name: "Performer 1", position: [0.23846114276669822, 0.7693022693022695] }, {
+      name: "Performer 9",
+      position: [0.6628104226020893, 0.5943962277295611]
+    }, { name: "Performer 10", position: [0.706834441278886, 0.6836792836792838] }, {
+      name: "Performer 11",
+      position: [0.7614306346945237, 0.7877621210954544]
+    }], duration: 2000, name: "", transitionBefore: { duration: 2000, midPoint: 0 }
+  }, {
+    dancers: [{
+      name: "Performer 10",
+      position: [0.7055555555555556, 0.5873015873015873]
+    }, { name: "Performer 9", position: [0.709837962962963, 0.40476190476190477] }, {
+      name: "Performer 7",
+      position: [0.6088966049382717, 0.3025168025168025]
+    }, { name: "Performer 6", position: [0.4982716049382716, 0.4044044044044044] }, {
+      name: "Performer 4",
+      position: [0.2848302469135803, 0.2030745030745031]
+    }, { name: "Performer 3", position: [0.2933950617283951, 0.38488488488488487] }, {
+      name: "Performer 2",
+      position: [0.28911265432098765, 0.5903045903045903]
+    }, { name: "Performer 11", position: [0.706983024691358, 0.783068783068783] }, {
+      name: "Performer 1",
+      position: [0.2907561728395061, 0.7906334906334906]
+    }, { name: "Performer 5", position: [0.39831018518518524, 0.30740740740740735] }, {
+      name: "Performer 8",
+      position: [0.711604938271605, 0.21465751465751462]
+    }], duration: 2000, name: "", transitionBefore: { duration: 2000, midPoint: 0 }
+  }]
 };
 
 export const demoChoreos = {
