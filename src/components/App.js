5a657f1d6584e3a7f59ee95fef1c1e34b3bbb54c
John Phua <phuajohn0@gmail.com>
2018-11-04 19:53:40 +0800

    Add animated canvas to landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import FormationScreen from "./Formation/FormationScreen";
 import { Route, Switch, Redirect } from "react-router-dom";
 import HomeScreen from "./Home/HomeScreen";
@@ -23,19 +23,19 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/dashboard" component={HomeScreen}/>
-          <Route exact path="/privacy" component={Privacy}/>
-          <Route exact path="/terms" component={Terms}/>
-          <Route exact path="/signup" component={SignUpScreen}/>
-          <Route exact path="/login" component={LogInScreen}/>
-          <Route exact path="/resetpassword" component={ResetPwScreen}/>
-          <Route exact path="/forgotpassword" component={ForgotPwScreen}/>
-          <Route exact path="/settings" component={Settings}/>
+          <Route exact path="/dashboard" component={HomeScreen} />
+          <Route exact path="/privacy" component={Privacy} />
+          <Route exact path="/terms" component={Terms} />
+          <Route exact path="/signup" component={SignUpScreen} />
+          <Route exact path="/login" component={LogInScreen} />
+          <Route exact path="/resetpassword" component={ResetPwScreen} />
+          <Route exact path="/forgotpassword" component={ForgotPwScreen} />
+          <Route exact path="/settings" component={Settings} />
           <Route exact path={`/choreo/:choreoId`} component={ChoreoHomeScreen} />
           <Route exact path={`/choreo/:choreoId/formation`} component={FormationScreen} />
           <Route exact path={`/choreoview/:choreoId/`} component={DancerFormationView} />
-          <Route exact path="/" component={LandingPageScreen}/>
-          <Redirect path="*" to="/"/>
+          <Route exact path="/" render={(props) => <LandingPageScreen {...props} choreoId={'demo'} />} />
+          <Redirect path="*" to="/" />
         </Switch>
       </div>
     );
