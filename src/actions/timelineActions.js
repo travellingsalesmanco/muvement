ee555d5c119085169cd13cf1525df3bc5d43e1b2
John Phua <phuajohn0@gmail.com>
2018-11-04 19:53:28 +0800

    Add loop and stop playing actions

--- src/actions/timelineActions.js
+++ src/actions/timelineActions.js
@@ -1,4 +1,9 @@
-import { EDIT_FORMATION_DURATION, EDIT_FORMATION_TRANSITION, TIMELINE_JUMP, TIMELINE_PAUSE } from "../constants/actionTypes";
+import {
+  EDIT_FORMATION_DURATION,
+  EDIT_FORMATION_TRANSITION,
+  TIMELINE_JUMP,
+  TIMELINE_PAUSE, TIMELINE_PLAY
+} from "../constants/actionTypes";
 import { getChoreo } from "../selectors/choreo";
 import { minFormationDuration, minTransitionDuration } from "../constants/defaults";
 import { truncToInterval, truncToPrecision } from "../lib/timelineUtils";
@@ -12,16 +17,51 @@ export function play(totalDuration, fps, speedup = 1) {
       if (getState().UI.isPlaying) {
         let newTime = getState().UI.elapsedTime + toAdvance
         if (newTime >= totalDuration) {
-          newTime = totalDuration
+          newTime = totalDuration;
           dispatch({ type: TIMELINE_PAUSE })
         }
         dispatch({
           type: TIMELINE_JUMP,
           payload: newTime,
-        })
+        });
         setTimeout(() => advanceNextFrame(), msPerFrame);
       }
-    }
+    };
+    advanceNextFrame();
+  }
+}
+
+export function stopPlaying() {
+  return (dispatch) => {
+    dispatch({ type: TIMELINE_PAUSE });
+    dispatch({
+      type: TIMELINE_JUMP,
+      payload: 0,
+    });
+  }
+}
+
+export function loop(totalDuration, fps, speedup = 1) {
+  const msPerFrame = truncToPrecision(1000 / fps);
+  const toAdvance = truncToPrecision(msPerFrame * speedup);
+  return (dispatch, getState) => {
+    dispatch({
+      type: TIMELINE_PLAY
+    });
+    const advanceNextFrame = () => {
+      if (getState().UI.isPlaying) {
+        let newTime = getState().UI.elapsedTime + toAdvance
+        if (newTime > totalDuration) {
+          // Loops back to start when completed
+          newTime = 0;
+        }
+        dispatch({
+          type: TIMELINE_JUMP,
+          payload: newTime,
+        });
+        setTimeout(() => advanceNextFrame(), msPerFrame);
+      }
+    };
     advanceNextFrame();
   }
 }
@@ -67,7 +107,7 @@ export function offsetTransitionBeforeDuration(choreoId, formationId, offsetDura
 export function jumpToTime(t) {
   t = truncToPrecision(t);
   return (dispatch, getState) => {
-    if(getState().UI.elapsedTime !== t) {
+    if (getState().UI.elapsedTime !== t) {
       dispatch({
         type: TIMELINE_JUMP,
         payload: t
